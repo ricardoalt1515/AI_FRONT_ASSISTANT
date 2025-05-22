e082b8d19d227b749becc19332970b7e1af844f4
ricardoalt1515 <ricardoaltamirano1515@gmail.com>
2025-05-22 08:52:30 -0700

    actualizacion backen URL para usar HTTTPS

--- next.config.ts
+++ next.config.ts
@@ -1,19 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   typescript: {
-    // ¡IMPORTANTE! Configuración que ignora errores de tipos durante el build
     ignoreBuildErrors: true,
   },
   reactStrictMode: true,
   env: {
-    BACKEND_URL: process.env.BACKEND_URL || 'https://gjyu74s2i.execute-api.us-east-1.amazonaws.com/prod',
+    BACKEND_URL: 'https://api.h2oassistant.com',
   },
-  // Proxy al API Gateway
   async rewrites() {
     return [
       {
         source: '/api/:path*',
-        destination: 'https://gjyu74s2i.execute-api.us-east-1.amazonaws.com/prod/api/:path*',
+        destination: 'https://api.h2oassistant.com/api/:path*',
       },
     ];
   },
